a233050ada2da3d559d073402af732f91f17162a
Nyedson Lorran <nyedsonlorranoficial@gmail.com>
2026-08-12 18:13:10 -0300

    basa da pagina do usuario

---
+++ meu-projeto-laravel/resources/views/HomeUsuario.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Turismo Paraíba</title>
+</head>
+
+<body>
+    <h1> teste sistema </h1>
+    <button>botão</button>
+</body>
+</html>
--- meu-projeto-laravel/routes/web.php
+++ meu-projeto-laravel/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('HomeUsuario');
 });
+
+Route::get('/', function () {
+    return view('HomeUsuario');
+});
+
