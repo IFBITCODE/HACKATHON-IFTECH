93933c43225ca5e5546b202338305c29caf9d56f
DiogorsLira <diogo.rslira@gmail.com>
2026-08-13 14:01:25 -0300

    implementa autenticação de login

--- iftech/routes/api.php
+++ iftech/routes/api.php
@@ -9,6 +9,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 use App\Http\Controllers\EmpreendedorController;
 
 Route::get('/empreendedores', [EmpreendedorController::class, 'index']);
